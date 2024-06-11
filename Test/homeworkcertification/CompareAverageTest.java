a2ae807261efe7a07899f613f152dab275eafe43
tatiana_sht <tatianaorlova0271@gmail.com>
2024-06-11 14:36:31 +1000

    Added solution hw6

--- Test/homeworkcertification/CompareAverageTest.java
+++ Test/homeworkcertification/CompareAverageTest.java
@@ -1,5 +1,6 @@
 package homeworkcertification;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,26 +8,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class CompareAverageListsTest {
+public class CompareAverageTest {
 
+    @DisplayName("Тест пустого листа")
     @Test
-    void calculatingAverageListWithoutNumbers() {
+    void testCalculatingAverageListWithoutNumbers() {
         List<Integer> listWithoutNumbers = new ArrayList<>();
         Average average = new Average(listWithoutNumbers);
         assertTrue(listWithoutNumbers.isEmpty());
         assertThat(average.avgValue()).isEqualTo(0);
     }
 
+    @DisplayName("Тест метода вычисления среднего значения")
     @Test
-    void calculatingAverageListNumbers() {
+    void testCalculatingAverageListNumbers() {
         List<Integer> listNumbers = new ArrayList<>(Arrays.asList(10, 18, 32, 25, 59, 17, 30));
         Average average = new Average(listNumbers);
         assertThat(average.avgValue()).isEqualTo(27.0);
     }
 
 
+    @DisplayName("Тест метода сравнения средних значений когда оно выше у первого листа")
     @Test
-    void comparisonOfAverageInFavorOfTheFirstList() {
+    void testComparisonOfAverageInFavorOfTheFirstList() {
         List<Integer> listOne = new ArrayList<>(Arrays.asList(10, 22, 33, 44, 55));
         List<Integer> listTwo = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
         Average averageOne = new Average(listOne);
@@ -37,9 +41,9 @@ public class CompareAverageListsTest {
                 .isEqualTo("Первый список имеет большее среднее значение.");
     }
 
-
+    @DisplayName("Тест метода сравнения средних значений когда оно выше у второго листа")
     @Test
-    void comparisonOfAverageInFavorOfTheSecondList() {
+    void testComparisonOfAverageInFavorOfTheSecondList() {
         List<Integer> listOne = new ArrayList<>(Arrays.asList(11, 12, 13, 14, 15));
         List<Integer> listTwo = new ArrayList<>(Arrays.asList(22, 32, 42, 52, 62));
         Average averageOne = new Average(listOne);
@@ -51,8 +55,9 @@ public class CompareAverageListsTest {
     }
 
 
+    @DisplayName("Тест метода сравнения средних значений когда они равны у обоих листов")
     @Test
-    void theAveragesOfBothListsAreEqual() {
+    void testTheAveragesOfBothListsAreEqual() {
         List<Integer> listOne = new ArrayList<>(Arrays.asList(11, 22, 33, 44, 55));
         List<Integer> listTwo = new ArrayList<>(Arrays.asList(11, 22, 33, 44, 55));
         Average averageOne = new Average(listOne);
